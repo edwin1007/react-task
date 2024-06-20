996b6000a250cfea53a56d8fcebeccc48f5b6fb9
edwin1007 <edwin.leonardo.cuaran@gmail.com>
2024-06-19 22:38:36 -0500

    primer commit

---
+++ src/data/task.js
@@ -0,0 +1,20 @@
+
+export const tasks = [
+    {
+        id:0,
+        title: 'mi primer tarea',
+        description: 'descripcion primer tarea'
+    },
+
+    {
+        id:1,
+        title: 'mi segunda tarea',
+        description: 'descripcion segunda tarea'
+    },
+
+    {
+        id:2,
+        title: 'mi tercer tarea',
+        description: 'descripcion tercer tarea'
+    }
+]
